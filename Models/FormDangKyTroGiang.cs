16760e4301ded99e49762ce86c82f7370612dd72
Duy Toan <duytoan2205@gmail.com>
2024-10-02 16:25:40 +0700

    Add function management form register teaching assistant

--- Models/FormDangKyTroGiang.cs
+++ Models/FormDangKyTroGiang.cs
@@ -33,5 +33,6 @@ namespace QuanLyCongTacTroGiangKhoaCNTT.Models
         public virtual Nganh Nganh { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<UngTuyenTroGiang> UngTuyenTroGiang { get; set; }
+        public virtual TaiKhoan TaiKhoan { get; set; }
     }
 }
