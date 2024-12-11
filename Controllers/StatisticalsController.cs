2653744ec6c6bacb89cdb617958aeb0c828a76d5
Huynh Quang <quang.197pm31195@vanlanguni.vn>
2024-12-11 22:59:23 +0700

    Add controller thong ke

---
+++ Controllers/StatisticalsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuanLyCongTacTroGiangKhoaCNTT.Controllers
+{
+    public class StatisticalsController : Controller
+    {
+        // GET: Statisticals
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
