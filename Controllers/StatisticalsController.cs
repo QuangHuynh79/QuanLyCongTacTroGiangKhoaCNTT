16d3a926a027e697ecd6820a5d9728db65a85dc6
Huynh Quang <quang.197pm31195@vanlanguni.vn>
2024-12-19 20:52:09 +0700

    Add function management statistical remuneration Build Version 4.0

--- Controllers/StatisticalsController.cs
+++ Controllers/StatisticalsController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Configuration;
 
 namespace QuanLyCongTacTroGiangKhoaCNTT.Controllers
 {
@@ -13,7 +14,7 @@ namespace QuanLyCongTacTroGiangKhoaCNTT.Controllers
         CongTacTroGiangKhoaCNTTEntities model = new CongTacTroGiangKhoaCNTTEntities();
 
         /// <summary>
-        /// Hàm này trả về view "Statistical" cho người dùng.
+        /// Hàm này trả về view "Statistical" thống kê trợ giảng cho người dùng.
         /// </summary>
         /// <returns>Trả về View "Statistical" với số liệu thống kê TA.</returns>
         [Authorize, BCNRole]
@@ -23,6 +24,16 @@ namespace QuanLyCongTacTroGiangKhoaCNTT.Controllers
         }
 
         /// <summary>
+        /// Hàm này trả về view "Statistical" thống kê thù lao cho người dùng.
+        /// </summary>
+        /// <returns>Trả về View "Statistical" với số liệu thống kê thù lao của TA.</returns>
+        [Authorize, BCNRole]
+        public ActionResult StatisticalRemuneration()
+        {
+            return View("StatisticalRemuneration", model.UngTuyenTroGiang.ToList());
+        }
+       
+        /// <summary>
         /// Hàm này trả về dữ liệu được lọc view "Statistical" cho người dùng.
         /// </summary>
         /// <returns>Trả về View "Statistical" với số liệu thống kê TA được lọc.</returns>
@@ -30,10 +41,52 @@ namespace QuanLyCongTacTroGiangKhoaCNTT.Controllers
         public ActionResult FilterStatistical(int hocky, int nganh)
         {
             var data = model.UngTuyenTroGiang.Where(w => w.TaiKhoan.ID_Nganh == nganh && w.LopHocPhan.ID_HocKy == hocky).ToList();
-            if (data.Count > 0) {
+            if (data.Count > 0)
+            {
                 var dataGroupBy = data.Where(w => w.TaiKhoan.ID_Nganh == nganh && w.LopHocPhan.ID_HocKy == hocky).GroupBy(g => g.ID_TaiKhoan).ToList();
             }
             return PartialView("_FilterStatistical", data);
+        }
+
+        /// <summary>
+        /// Hàm này trả về dữ liệu được lọc view "FilterRemuneration" thống kê thù lao cho người dùng.
+        /// </summary>
+        /// <returns>Trả về View "FilterRemuneration" với số liệu thống kê thù lao được lọc.</returns>
+        [Authorize, BCNRole]
+        [HttpPost]
+        public ActionResult FilterRemuneration(int hocky, int nganh)
+        {
+            var data = model.UngTuyenTroGiang.Where(w => w.TaiKhoan.ID_Nganh == nganh && w.LopHocPhan.ID_HocKy == hocky).ToList();
+            if (data.Count > 0)
+            {
+                var dataGroupBy = data.Where(w => w.TaiKhoan.ID_Nganh == nganh && w.LopHocPhan.ID_HocKy == hocky).GroupBy(g => g.ID_TaiKhoan).ToList();
+            }
+            return PartialView("_FilterRemuneration", data);
+        }
+
+        /// <summary>
+        /// Hàm này dùng để lưu cập nhật giá thù lao.
+        /// </summary>
+        /// <returns>Trả về trạng thái đã lưu thành công hay lỗi thất bại.</returns>
+        [Authorize, BCNRole]
+        public ActionResult UpdateRemuneratiion(string thulao)
+        {
+            try
+            {
+                Configuration objConfig = System.Web.Configuration.WebConfigurationManager.OpenWebConfiguration("~");
+                AppSettingsSection objAppsettings = (AppSettingsSection)objConfig.GetSection("appSettings");
+                if (objAppsettings != null)
+                {
+                    objAppsettings.Settings["RemunerationPrice"].Value = thulao;
+                    objConfig.Save();
+                }
+                return Content("SUCCESS");
+            }
+            catch (Exception Ex)
+            {
+                return Content("Chi tiết lỗi " + Ex.Message);
+            }
+
         }
     }
 }
---
+++ Validation/department/statisticals/thulao/cauhinh.js
@@ -0,0 +1,86 @@
+﻿$(document).ready(function () {
+    $('body').on('click', '[id="btncauhinhthulao"]', function () {
+        var thulao = $('body').find('[id="giathulao"]').attr('defaultData');
+        $('body').find('[id="giathulao"]').val(thulao);
+        $('body').find('[id="valid-giathulao"]').text('');
+        $('body').find('[id="cauhinh"]').modal('toggle');
+    });
+
+    $('body').on('click', '[id="btnSubmitThuLao"]', function () {
+        var btn = $(this);
+        btn.html('<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"> </span> Vui lòng chờ...');
+        btn.prop('disabled', true);
+        $('body').find('[id="btnThuLaoClose"]').prop('disabled', true);
+
+        var thulao = $('body').find('[id="giathulao"]').val();
+        var validthulao = $('body').find('[id="valid-giathulao"]');
+        validthulao.text('');
+
+        var check = true;
+
+        if (thulao.length < 1) {
+            check = false;
+
+            btn.html('Lưu thông tin');
+            btn.prop('disabled', false);
+            $('body').find('[id="btnThuLaoClose"]').prop('disabled', false);
+
+            validthulao.text("Vui lòng không bỏ trống giá thù lao.");
+            $('body').find('[id="giathulao"]').focus();
+        }
+        else if (Number(thulao.replace(/,/g, '')) <= 0) {
+            check = false;
+
+            btn.html('Lưu thông tin');
+            btn.prop('disabled', false);
+            $('body').find('[id="btnThuLaoClose"]').prop('disabled', false);
+
+            validthulao.text("Giá thù lao phải lớn hơn 0 VNĐ");
+            $('body').find('[id="giathulao"]').focus();
+        }
+
+        if (check == true) {
+            var formData = new FormData();
+            formData.append('thulao', thulao.replace(/,/g, ''));
+
+            $.ajax({
+                error: function (a, xhr, c) { if (a.status == 403 && a.responseText.indexOf("SystemLoginAgain") != -1) { window.location.href = $('body').find('[id="requestPath"]').val() + "account/signin"; } },
+                url: $('#requestPath').val() + "Statisticals/UpdateRemuneratiion",
+                data: formData,
+                dataType: 'html',
+                type: 'POST',
+                processData: false,
+                contentType: false,
+            }).done(function (ketqua) {
+                $('body').find('[id="cauhinh"]').modal('toggle');
+
+                if (ketqua == "SUCCESS") {
+                    btn.html('Lưu thông tin');
+                    btn.prop('disabled', false);
+                    $('body').find('[id="btnThuLaoClose"]').prop('disabled', false);
+                    $('body').find('[id="giathulao"]').attr('defaultData', thulao);
+
+                    Toast.fire({
+                        icon: "success",
+                        title: "Đã cập nhật giá thù lao."
+                    }).then(() => {
+                        window.location.reload();
+                    });
+                }
+                else {
+                    btn.html('Lưu thông tin');
+                    btn.prop('disabled', false);
+                    $('body').find('[id="btnThuLaoClose"]').prop('disabled', false);
+
+                    Toast.fire({
+                        icon: "error",
+                        title: ketqua
+                    }).then(() => {
+                        window.location.reload();
+                    });
+                    
+                }
+            });
+        }
+    });
+});
